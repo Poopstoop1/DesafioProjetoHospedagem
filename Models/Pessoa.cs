8b01e0cbb6e7b2d6bcc1c18b8cd029e94190be8b
Poopstoop1 <daniel.026@hotmail.com>
2024-05-20 11:22:49 -0300

    Primeiro Commit

---
+++ Models/Pessoa.cs
@@ -0,0 +1,16 @@
+namespace DesafioProjetoHospedagem.Models;
+
+public class Pessoa
+{
+
+
+    public Pessoa(string nome, string sobrenome)
+    {
+        Nome = nome;
+        Sobrenome = sobrenome;
+    }
+
+    public string Nome { get; set; }
+    public string Sobrenome { get; set; }
+   
+}
